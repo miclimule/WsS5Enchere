957c9ce5539e936909fd0f90d7d6b3f7d2b02488
Milclimule <99533207+miclimule@users.noreply.github.com>
2023-02-03 20:48:06 +0300

    e

--- src/main/java/com/example/demo/service/Service.java
+++ src/main/java/com/example/demo/service/Service.java
@@ -416,7 +416,7 @@ public class Service {
 	
 	@GetMapping("/getSolde")
 	private List<Solde_client> getSolde(int id) {
-		return db.query("select idClient , sum(montant) from solde_client where isvalidate = 1 and idClient = "+id+" group by idClient", new BeanPropertyRowMapper<Solde_client>(Solde_client.class)); 
+		return db.query("select idClient , sum(montant) as montant from solde_client where isvalidate = 1 and idClient = "+id+" group by idClient", new BeanPropertyRowMapper<Solde_client>(Solde_client.class)); 
 	}
 	
 //	@GetMapping("/json-to-object")
